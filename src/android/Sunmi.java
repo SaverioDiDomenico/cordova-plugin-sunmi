2d4407387153992a64898756830abfb61ea96e51
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2019-02-13 15:00:26 +0100

    Update Sunmi.java

--- src/android/Sunmi.java
+++ src/android/Sunmi.java
@@ -20,73 +20,99 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 
 public class Sunmi extends CordovaPlugin {
-    private static final String TAG = "Sunmi";
+	private static final String TAG = "Sunmi";
 
-    /**
-     * Sets the context of the Command. This can then be used to do things like
-     * get file paths associated with the Activity.
-     *
-     * @param cordova The context of the main Activity.
-     * @param webView The CordovaWebView Cordova is running in.
-     */
-    @Override
-    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
+	/**
+	 * Sets the context of the Command. This can then be used to do things like
+	 * get file paths associated with the Activity.
+	 *
+	 * @param cordova The context of the main Activity.
+	 * @param webView The CordovaWebView Cordova is running in.
+	 */
+	@Override
+	public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
 
-    }
+	}
 
-    /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action            The action to execute.
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback id used when calling back into JavaScript.
-     * @return                  True if the action was valid, false otherwise.
-     */
-    @Override
-    public boolean execute(final String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
-        final Activity activity = this.cordova.getActivity();
-        final Window window = activity.getWindow();
+	/**
+	 * Executes the request and returns PluginResult.
+	 *
+	 * @param action            The action to execute.
+	 * @param args              JSONArry of arguments for the plugin.
+	 * @param callbackContext   The callback id used when calling back into JavaScript.
+	 * @return                  True if the action was valid, false otherwise.
+	 */
+	@Override
+	public boolean execute(final String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+		final Activity activity = this.cordova.getActivity();
+		final Window window = activity.getWindow();
 
 		if ("sendText".equals(action)) {
 			String content = args.getString(0);
-		     float size =  Integer.parseInt(args.getString(1));
-		     boolean isBold = Boolean.parseBoolean(args.getString(2));
-		     boolean isUnderLine = Boolean.parseBoolean(args.getString(3));
+			float size =  Integer.parseInt(args.getString(1));
+			boolean isBold = Boolean.parseBoolean(args.getString(2));
+			boolean isUnderLine = Boolean.parseBoolean(args.getString(3));
 
-      //Get Context to send to print service
-       Context context=this.cordova.getActivity().getApplicationContext();
-       AidlUtil.getInstance().connectPrinterService(context);
+			//Get Context to send to print service
+			Context context=this.cordova.getActivity().getApplicationContext();
+			AidlUtil.getInstance().connectPrinterService(context);
+			try{
+				Thread.sleep(250);
+				AidlUtil.getInstance().connectPrinterService(context);
+			}catch (Exception ex1) {
+				// TODO: handle exception
+			}
 
-       AidlUtil.getInstance().initPrinter();
-			 AidlUtil.getInstance().printText(content, size, isBold, isUnderLine);
-			 callbackContext.success();
-		    return true;
+			AidlUtil.getInstance().initPrinter();			
+			try{
+				Thread.sleep(250);
+				AidlUtil.getInstance().initPrinter();
+			}catch (Exception ex1) {
+				// TODO: handle exception
+			}
+			
+			AidlUtil.getInstance().printText(content, size, isBold, isUnderLine);
+			callbackContext.success();
+			return true;
 		}
 
-      if ("sendImage".equals(action)) {
-        //get image
-        String content = args.getString(0);
-        String image = content.split("base64,")[1];
+		if ("sendImage".equals(action)) {
+			//get image
+			String content = args.getString(0);
+			String image = content.split("base64,")[1];
 
-        //Get Context to send to printer service
-        Context context=this.cordova.getActivity().getApplicationContext();
-        AidlUtil.getInstance().connectPrinterService(context);
+			//Get Context to send to printer service
+			Context context=this.cordova.getActivity().getApplicationContext();
+			AidlUtil.getInstance().connectPrinterService(context);
+			try{
+				Thread.sleep(250);
+				AidlUtil.getInstance().connectPrinterService(context);
+			}catch (Exception ex1) {
+				// TODO: handle exception
+			}
+			
 
-        //decode to bitmap
-        byte[] decodedString = Base64.decode(image, Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+			//decode to bitmap
+			byte[] decodedString = Base64.decode(image, Base64.DEFAULT);
+			Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        //send to printer
-        AidlUtil.getInstance().initPrinter();
-        AidlUtil.getInstance().printBitmap(bitmap);
+			//send to printer
+			AidlUtil.getInstance().initPrinter();
+			try{
+				Thread.sleep(250);
+				AidlUtil.getInstance().initPrinter();
+			}catch (Exception ex1) {
+				// TODO: handle exception
+			}
+			AidlUtil.getInstance().printBitmap(bitmap);
 
-        callbackContext.success();
-        return true;
-      }
+			callbackContext.success();
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
 
 
